3a8d7c115e9c6028f476ff02bc3e64a09b59a7aa
sww1235 <sww1235@gmail.com>
2016-07-22 10:14:36 -0600

    updating ingredient struct

---
+++ ingredient.go
@@ -0,0 +1,9 @@
+package recipeDatabase
+
+//The Ingredient struct stores data for a particular Ingredient
+//used in a recipe
+type Ingredient struct {
+	Name           string
+	UPC            string
+	QuantityNeeded int
+}
