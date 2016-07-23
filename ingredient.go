cd66b9dbb025a1689262250140db4bc3118d67ff
sww1235 <sww1235@gmail.com>
2016-07-22 22:59:03 -0600

    added more content

--- ingredient.go
+++ ingredient.go
@@ -3,7 +3,15 @@ package recipeDatabase
 //The Ingredient struct stores data for a particular Ingredient
 //used in a recipe
 type Ingredient struct {
-	Name           string
-	UPC            string
-	QuantityNeeded int
+	Name               string
+	UPC                string
+	QuantityNeeded     int
+	IngredientUnit     ingredientUnit
+	InDatabase         bool
+	QuantityInDatabase int
 }
+
+//Type ingredientUnit can be any value but only certain values are
+//able to be converted at present. Values that are unable to be converted
+//will not be able to be selected.
+type ingredientUnit string
--- recipe.go
+++ recipe.go
@@ -2,15 +2,30 @@ package recipeDatabase
 
 import "time"
 
+//TODO: look at adding equipment/pots/pans to Recipe struct
+
 //A Recipe struct is the internal representation of a recipe JSON file
 type Recipe struct {
+	Name              string
 	Ingredients       []Ingredient
 	QuantityMade      int
 	QuantityMadeUnits string
 	Steps             []step
+	TotalPrepTime     time.Duration
+	TotalCookTime     time.Duration
+	TotalWaitTime     time.Duration
+	TotalOtherTime    time.Duration
+	TotalTime         time.Duration
+	Tags              []string
 }
 
+//StepType has 4 recognized values, prep, cook, wait
+//and then anything else as other. This selects what duration in a Recipe
+//TimeNeeded is added to.
+type StepType string
 type step struct {
-	TimeNeeded  time.Duration
-	Temperature temperature
+	TimeNeeded   time.Duration
+	StepType     StepType
+	Temperature  temperature
+	Instructions string
 }
