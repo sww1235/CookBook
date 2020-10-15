34afd0915f8a2a428dbd36764a5b40f75bf800c0
Stephen Walker-Weinshenker <sww1235@gmail.com>
2020-10-14 19:14:20 -0600

    Fixed temperature sorting, instead of name, value

--- temperature.go
+++ temperature.go
@@ -14,17 +14,8 @@ func (t Temperature) String() string {
 // ByName implements sort.Interface for []Temperature
 // based on the Name field
 // https://golang.org/pkg/sort/#pkg-overview
-type ByName []Temperature
+type ByValueT []Temperature
 
-func (n ByName) Len() int           { return len(n) }
-func (n ByName) Swap(i, j int)      { n[i], n[j] = n[j], n[i] }
-func (n ByName) Less(i, j int) bool { return n[i].Name < n[j] }
-
-// ByID implements sort.Interface for []Temperature
-// based on the ID field
-// https://golang.org/pkg/sort/#pkg-overview
-type ByID []Temperature
-
-func (d ByID) Len() int           { return len(d) }
-func (d ByID) Swap(i, j int)      { d[i], d[j] = d[j], d[i] }
-func (d ByID) Less(i, j int) bool { return d[i].ID < d[j].ID }
+func (t ByValueT) Len() int           { return len(t) }
+func (t ByValueT) Swap(i, j int)      { t[i], t[j] = t[j], t[i] }
+func (t ByValueT) Less(i, j int) bool { return t[i].Value < t[j].Value }
